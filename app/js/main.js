d0856ab603adf25096be83524143f17319cb3897
RashadNovruzov <r4shad.novruz0v@gmail.com>
2022-07-27 18:45:58 +0400

    section blog is ready

--- app/js/main.js
+++ app/js/main.js
@@ -74,10 +74,37 @@ $(document).ready(function () {
                     slidesToScroll: 1
                 }
             }
-            
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
+        ]
+    });
+    $(".blog__items").slick({
+        arrows:false,
+        slidesToShow:4,
+        dots:true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3.2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2.9,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 2.4,
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
         ]
     });
 });
